1203881594f0cee5d957c5c5039cfd95e3d13e29
Alan Ren <10086000+alanvww@users.noreply.github.com>
2021-11-30 08:25:08 -0500

    create .env

---
+++ src/components/FirebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const FirebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: 'exercise-5-fall-2021.firebaseapp.com',
+	projectId: 'exercise-5-fall-2021',
+	storageBucket: 'exercise-5-fall-2021.appspot.com',
+	messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASURE_ID,
+};
+
+export default FirebaseConfig;
